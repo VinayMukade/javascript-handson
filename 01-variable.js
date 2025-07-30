309ec7cbf587c4b3d890c7df1d4f93702b927cd7
VinayMukade <vinaymukade55@gmail.com>
2025-07-30 07:37:31 +0530

    New Updated Data
    New Updated Data

---
+++ 01-variable.js
@@ -0,0 +1,16 @@
+// Variable Declaration
+
+var City = "Latur";
+console.log("My city name is: " + City);
+
+var fname = "Raghav";
+console.log("My best friend name is:" + fname);
+
+var country = "India";
+console.log("My country name is:" + country);
+
+var schoolName = "Mahatma Gandhi English School";
+console.log("My school name is:" + schoolName);
+
+var coll = "DY Patil University";
+console.log("My collage name is:"+coll);
